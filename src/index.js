6e0e1aa0752fb21e2d5e6af823a728db3e30f99e
tuanthanhk428@gmail.com <thanh30012002>
2025-04-16 19:06:24 +0700

    fix-cors lan 3

--- src/index.js
+++ src/index.js
@@ -36,18 +36,6 @@ app.use(
     credentials: true,
   })
 );
-
-  app.use(cors({
-    origin: function (origin, callback) {
-      // Cho phép gọi từ Postman hoặc Curl (khi origin là undefined)
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true
-  }));
-  
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(bodyParser.json())
